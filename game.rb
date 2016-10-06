ede8832b0b19fee415cac4c3e4a9b10e393ad7e3
emmahern <emmahern.comms@gmail.com>
2016-10-05 20:02:22 -0500

    Update game.rb
    simulation of second developer

--- game.rb
+++ game.rb
@@ -4,8 +4,11 @@ puts "This is way better and I like it"
 
 puts "Line #3 is interesting."
 
+puts "I'm adding something to this file"
+
 puts "This is crazy b/c I am using a new feature"
 
 a = gets
 
 p a
+
