a815de7189c6b844b2c78cec99ae7a1bf83562e8
EMMA HERNANDEZ <EMMAHERN.COMMS@GMAIL.COM>
2016-10-05 19:39:21 -0500

    THis is way better and I like it.

---
+++ game.rb
@@ -0,0 +1,11 @@
+puts "This is only the beginning
+
+puts "This is way better and I like it"
+
+puts "Line #3 is interesting."
+
+puts "This is crazy b/c I am using a new feature"
+
+a = gets
+
+p a
